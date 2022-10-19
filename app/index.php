b636e361c5574e23c482b771470c05eec3506686
MarcQueral <mqueral@iesebre.com>
2022-10-19 13:50:40 +0200

    Video intorducció PHP #1 acabat

--- app/index.php
+++ app/index.php
@@ -1,3 +1,29 @@
 <?php
 
-$greeting = 'Hola mon!';
+require 'app/helpers.php';
+
+require 'app/Task.php';
+
+//$task = new Task(1,'comprar pa','a la panaderia',0);
+//var_dump($task);
+
+$user = 'debian-sys-maint';
+$pass = 'GwO74lB1ccQCHV7w';
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=phplaraveldevs', $user, $pass);
+} catch (\Exception $e) {
+    echo 'Error de connexió a la base de dades';
+}
+
+$statement = $dbh->prepare('SELECT * FROM tasks;');
+
+$statement->execute();
+
+$tasks = $statement->fetchAll(PDO::FETCH_CLASS,'Task');
+
+var_dump($tasks);
+
+$greeting = greet();
+
+//$greeting = 'Hola ' . $_GET['name'] . ' ' . $_GET['name'] . '!';
