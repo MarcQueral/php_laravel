7d56ee908608cbe9dfa825b42ba29b90768c86fc
MarcQueral <mqueral@iesebre.com>
2022-11-12 11:50:55 +0100

    Video 2 acabat

---
+++ framework/Database/Database.php
@@ -0,0 +1,21 @@
+<?php
+
+class Database
+{
+    public $config;
+
+
+    public function __construct($config)
+    {
+        $this->config = $config;
+        $this->connection = new Connection($config);
+    }
+
+    function selectAll($table) {
+        return fetchAllTasks($this->connection->connectDB($this->config));
+    }
+
+    function insert() {
+
+    }
+}
